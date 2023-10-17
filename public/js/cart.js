2920fa5143b3380a915763b673a2c78e69983563
Helekin <clsjs94@hotmail.com>
2023-10-17 12:54:24 -0500

    localstorage cart

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,8 +1,16 @@
+document.querySelectorAll("[data-product]").forEach(function (button) {
+  button.addEventListener("click", function (event) {
+    const product = JSON.parse(event.target.getAttribute("data-product"));
+    const qty = document.getElementById("qty").value;
+    addToCart(product, qty);
+  });
+});
+
 function addDecimals(num) {
   return (Math.round(num * 100) / 100).toFixed(2);
 }
 
-function addToCart(product) {
+function addToCart(product, qty) {
   let cart = localStorage.getItem("cart");
   cart = cart
     ? JSON.parse(cart)
@@ -12,14 +20,14 @@ function addToCart(product) {
 
   if (existItem) {
     cart.cartItems = cart.cartItems.map((item) =>
-      item.id === existItem.id ? product : item
+      item.id === existItem.id ? { ...product, qty: Number(qty) } : item
     );
   } else {
-    cart.cartItems = [...cart.cartItems, product];
+    cart.cartItems = [...cart.cartItems, { ...product, qty: Number(qty) }];
   }
 
   cart.itemsPrice = addDecimals(
-    cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+    cart.cartItems.reduce((acc, item) => acc + item.price * qty, 0)
   );
 
   cart.shippingPrice = addDecimals(cart.itemsPrice > 100 ? 0 : 10);
@@ -33,4 +41,24 @@ function addToCart(product) {
   ).toFixed(2);
 
   localStorage.setItem("cart", JSON.stringify(cart));
+
+  updateCartCount();
 }
+
+function updateCartCount() {
+  let cart = JSON.parse(localStorage.getItem("cart"));
+
+  let totalCount = 0;
+
+  if (cart && cart.cartItems) {
+    totalCount = cart.cartItems.reduce((count, item) => count + item.qty, 0);
+  }
+
+  const cartCountElement = document.getElementById("cart-count");
+
+  if (cartCountElement) {
+    cartCountElement.textContent = totalCount.toString();
+  }
+}
+
+updateCartCount();
